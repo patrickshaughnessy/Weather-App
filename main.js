e65f86e7e8605d1dede1bf43ba8c56b2ce2c45e1
Patrick Shaughnessy <pshaughn86@gmail.com>
2015-11-04 08:54:18 -0800

    persist data from local storage on page load

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 (function($){
 'use strict';
 
-  var apiURL = 'http://api.wunderground.com/api/c0b168fbe50bdfd7/';
+  let apiURL = 'http://api.wunderground.com/api/c0b168fbe50bdfd7/';
 
-  // var pageData = localStorage.pageData ? JSON.parse(localStorage.pageData) : [];
-  var pageData = [];
+  let pageData = localStorage.pageData ? JSON.parse(localStorage.pageData) : [];
+  // var pageData = [];
 
   $(document).ready(init);
 
   function init(){
-
-    let userLocationURL = apiURL + 'geolookup/q/autoip.json';
 
     $('#changeLocationButton').click(changeLocation);
     $('#toggleFC').click(toggleFC);
     $('#compareCityOne').click(compareCityOne);
     // $('#compareCityTwo button').click(compareCityTwo);
     // $('#compareCityThree button').click(compareCityThree);
+
+    if (!pageData.length){
+      initializeWithUserLocation();
+    } else {
+      populateValuesOnPage();
+    }
+
+
+
+  }
+
+  function initializeWithUserLocation(){
+
+    let userLocationURL = apiURL + 'geolookup/q/autoip.json';
 
     $.get(userLocationURL)
     .done(function(data){
@@ -130,7 +142,7 @@
 
               pageData.push(almanacData);
 
-              // updateLocalStorage();
+              updateLocalStorage();
               populateValuesOnPage();
 
 
@@ -161,168 +173,171 @@
       this.forecastText = data.forecast.txt_forecast.forecastday[i*2].fcttext;
     }
 
-    function updateLocalStorage() {
-      localStorage.pageData = JSON.stringify(pageData);
-    }
+  }
 
-    function populateValuesOnPage() {
-      // debugger;
-      console.log(pageData);
+  function updateLocalStorage() {
+    localStorage.pageData = JSON.stringify(pageData);
+  }
 
-      $('#userLocation').text(pageData[0].userCity + ", " + pageData[0].userState);
-      $('#currentWeatherIcon').attr('src', pageData[1].weatherIconURL);
+  function populateValuesOnPage() {
+    // debugger;
+    console.log(pageData);
+
+    $('#userLocation').text(pageData[0].userCity + ", " + pageData[0].userState);
+    $('#currentWeatherIcon').attr('src', pageData[1].weatherIconURL);
 
 // refactor to loop if time
-      let $forecastIcon1 = $('<img>').attr('src', pageData[2].firstDay.icon);
-      $('#forecastDayOne h3').text(pageData[2].firstDay.weekday);
-      $('#forecastDayOne h1').text(pageData[2].firstDay.conditions);
-      $('#forecastDayOne h1').append($forecastIcon1);
-      $('#forecastDayOne p').text(pageData[2].firstDay.forecastText);
+    let $forecastIcon1 = $('<img>').attr('src', pageData[2].firstDay.icon);
+    $('#forecastDayOne h3').text(pageData[2].firstDay.weekday);
+    $('#forecastDayOne h1').text(pageData[2].firstDay.conditions);
+    $('#forecastDayOne h1').append($forecastIcon1);
+    $('#forecastDayOne p').text(pageData[2].firstDay.forecastText);
 
-      let $forecastIcon2 = $('<img>').attr('src', pageData[2].secondDay.icon);
-      $('#forecastDayTwo h3').text(pageData[2].secondDay.weekday);
-      $('#forecastDayTwo h1').text(pageData[2].secondDay.conditions);
-      $('#forecastDayTwo h1').append($forecastIcon2);
-      $('#forecastDayTwo p').text(pageData[2].secondDay.forecastText);
+    let $forecastIcon2 = $('<img>').attr('src', pageData[2].secondDay.icon);
+    $('#forecastDayTwo h3').text(pageData[2].secondDay.weekday);
+    $('#forecastDayTwo h1').text(pageData[2].secondDay.conditions);
+    $('#forecastDayTwo h1').append($forecastIcon2);
+    $('#forecastDayTwo p').text(pageData[2].secondDay.forecastText);
 
-      let $forecastIcon3 = $('<img>').attr('src', pageData[2].thirdDay.icon);
-      $('#forecastDayThree h3').text(pageData[2].thirdDay.weekday);
-      $('#forecastDayThree h1').text(pageData[2].thirdDay.conditions);
-      $('#forecastDayThree h1').append($forecastIcon3);
-      $('#forecastDayThree p').text(pageData[2].thirdDay.forecastText);
+    let $forecastIcon3 = $('<img>').attr('src', pageData[2].thirdDay.icon);
+    $('#forecastDayThree h3').text(pageData[2].thirdDay.weekday);
+    $('#forecastDayThree h1').text(pageData[2].thirdDay.conditions);
+    $('#forecastDayThree h1').append($forecastIcon3);
+    $('#forecastDayThree p').text(pageData[2].thirdDay.forecastText);
 
-      let $forecastIcon4 = $('<img>').attr('src', pageData[2].fourthDay.icon);
-      $('#forecastDayFour h3').text(pageData[2].fourthDay.weekday);
-      $('#forecastDayFour h1').text(pageData[2].fourthDay.conditions);
-      $('#forecastDayFour h1').append($forecastIcon4);
-      $('#forecastDayFour p').text(pageData[2].fourthDay.forecastText);
+    let $forecastIcon4 = $('<img>').attr('src', pageData[2].fourthDay.icon);
+    $('#forecastDayFour h3').text(pageData[2].fourthDay.weekday);
+    $('#forecastDayFour h1').text(pageData[2].fourthDay.conditions);
+    $('#forecastDayFour h1').append($forecastIcon4);
+    $('#forecastDayFour p').text(pageData[2].fourthDay.forecastText);
 
-      // F or C if conditional
+    // F or C if conditional
+    if (pageData[0].userTempType === 'F'){
+      $('#currentTemp').text(pageData[1].currentTempF.toString() + ' °F');
+      $('#feelsLike').text('Feels Like: ' + pageData[1].feelsLikeF + ' °F');
+
+      $('#forecastDayOne h4:first').text('high: ' + pageData[2].firstDay.highF + ' °F');
+      $('#forecastDayOne h4:last').text('low: ' + pageData[2].firstDay.lowF + ' °F');
+      $('#forecastDayTwo h4:first').text('high: ' + pageData[2].secondDay.highF + ' °F');
+      $('#forecastDayTwo h4:last').text('low: ' + pageData[2].secondDay.lowF + ' °F');
+      $('#forecastDayThree h4:first').text('high: ' + pageData[2].thirdDay.highF + ' °F');
+      $('#forecastDayThree h4:last').text('low: ' + pageData[2].thirdDay.lowF + ' °F');
+      $('#forecastDayFour h4:first').text('high: ' + pageData[2].fourthDay.highF + ' °F');
+      $('#forecastDayFour h4:last').text('low: ' + pageData[2].fourthDay.lowF + ' °F');
+
+      $('#lastYearInfo').text('Last year on ' + pageData[3].lastYearDatePretty + ', it was ' + pageData[3].lastYearTempF + ' °F');
+
+      $('#recordHigh').text('The highest recorded temperature for today was ' + pageData[4].recordHighTempF + '°F in ' + pageData[4].recordHighYear);
+      $('#recordLow').text('The lowest recorded temperature for today was ' + pageData[4].recordLowTempF  + '°F in ' + pageData[4].recordLowYear);
+    } else {
+      $('#toggleFC').removeClass('btn-info').addClass('btn-warning');
+
+      $('#currentTemp').text(pageData[1].currentTempC.toString() + ' °C');
+      $('#feelsLike').text('Feels Like: ' + pageData[1].feelsLikeC + ' °C');
+
+      $('#forecastDayOne h4:first').text('high: ' +pageData[2].firstDay.highC + ' °C');
+      $('#forecastDayOne h4:last').text('low: ' + pageData[2].firstDay.lowC + ' °C');
+      $('#forecastDayTwo h4:first').text('high: ' +pageData[2].secondDay.highC + ' °C');
+      $('#forecastDayTwo h4:last').text('low: ' + pageData[2].secondDay.lowC + ' °C');
+      $('#forecastDayThree h4:first').text('high: ' +pageData[2].thirdDay.highC + ' °C');
+      $('#forecastDayThree h4:last').text('low: ' + pageData[2].thirdDay.lowC + ' °C');
+      $('#forecastDayFour h4:first').text('high: ' +pageData[2].fourthDay.highC + ' °C');
+      $('#forecastDayFour h4:last').text('low: ' + pageData[2].fourthDay.lowC + ' °C');
+
+      $('#lastYearInfo').text('On ' + pageData[3].lastYearDatePretty + ', it was ' + pageData[3].lastYearTempC + '°C');
+
+      $('#recordHigh').text('The highest recorded temperature for today was ' + pageData[4].recordHighTempC + '°C in ' + pageData[4].recordHighYear);
+      $('#recordLow').text('The lowest recorded temperature for today was ' + pageData[4].recordLowTempC  + '°C in ' + pageData[4].recordLowYear);
+    }
+
+    if (pageData[5]){
+      $('#cityOneLocation').show();
+      $('#cityOneLocation h3').text('in ' + pageData[5].cityOne + ', ' + pageData[5].stateOne + ' right now');
+
       if (pageData[0].userTempType === 'F'){
-        $('#currentTemp').text(pageData[1].currentTempF.toString() + ' °F');
-        $('#feelsLike').text('Feels Like: ' + pageData[1].feelsLikeF + ' °F');
-
-        $('#forecastDayOne h4:first').text('high: ' + pageData[2].firstDay.highF + ' °F');
-        $('#forecastDayOne h4:last').text('low: ' + pageData[2].firstDay.lowF + ' °F');
-        $('#forecastDayTwo h4:first').text('high: ' + pageData[2].secondDay.highF + ' °F');
-        $('#forecastDayTwo h4:last').text('low: ' + pageData[2].secondDay.lowF + ' °F');
-        $('#forecastDayThree h4:first').text('high: ' + pageData[2].thirdDay.highF + ' °F');
-        $('#forecastDayThree h4:last').text('low: ' + pageData[2].thirdDay.lowF + ' °F');
-        $('#forecastDayFour h4:first').text('high: ' + pageData[2].fourthDay.highF + ' °F');
-        $('#forecastDayFour h4:last').text('low: ' + pageData[2].fourthDay.lowF + ' °F');
-
-        $('#lastYearInfo').text('Last year on ' + pageData[3].lastYearDatePretty + ', it was ' + pageData[3].lastYearTempF + ' °F');
-
-        $('#recordHigh').text('The highest recorded temperature for today was ' + pageData[4].recordHighTempF + '°F in ' + pageData[4].recordHighYear);
-        $('#recordLow').text('The lowest recorded temperature for today was ' + pageData[4].recordLowTempF  + '°F in ' + pageData[4].recordLowYear);
+        $('#cityOneLocation h1').text(pageData[6].currentTempFOne + '°F');
+        $('#cityOneLocation h4').text('Feels like ' + pageData[6].feelsLikeFOne + '°F');
       } else {
-        $('#currentTemp').text(pageData[1].currentTempC.toString() + ' °C');
-        $('#feelsLike').text('Feels Like: ' + pageData[1].feelsLikeC + ' °C');
-
-        $('#forecastDayOne h4:first').text('high: ' +pageData[2].firstDay.highC + ' °C');
-        $('#forecastDayOne h4:last').text('low: ' + pageData[2].firstDay.lowC + ' °C');
-        $('#forecastDayTwo h4:first').text('high: ' +pageData[2].secondDay.highC + ' °C');
-        $('#forecastDayTwo h4:last').text('low: ' + pageData[2].secondDay.lowC + ' °C');
-        $('#forecastDayThree h4:first').text('high: ' +pageData[2].thirdDay.highC + ' °C');
-        $('#forecastDayThree h4:last').text('low: ' + pageData[2].thirdDay.lowC + ' °C');
-        $('#forecastDayFour h4:first').text('high: ' +pageData[2].fourthDay.highC + ' °C');
-        $('#forecastDayFour h4:last').text('low: ' + pageData[2].fourthDay.lowC + ' °C');
-
-        $('#lastYearInfo').text('On ' + pageData[3].lastYearDatePretty + ', it was ' + pageData[3].lastYearTempC + '°C');
-
-        $('#recordHigh').text('The highest recorded temperature for today was ' + pageData[4].recordHighTempC + '°C in ' + pageData[4].recordHighYear);
-        $('#recordLow').text('The lowest recorded temperature for today was ' + pageData[4].recordLowTempC  + '°C in ' + pageData[4].recordLowYear);
+        $('#cityOneLocation h1').text(pageData[6].currentTempCOne + '°C');
+        $('#cityOneLocation h4').text('Feels like ' + pageData[6].feelsLikeCOne + '°C');
       }
-
-      if (pageData[5]){
-        $('#cityOneLocation').show();
-        $('#cityOneLocation h3').text('in ' + pageData[5].cityOne + ', ' + pageData[5].stateOne + ' right now');
-
-        if (pageData[0].userTempType === 'F'){
-          $('#cityOneLocation h1').text(pageData[6].currentTempFOne + '°F');
-          $('#cityOneLocation h4').text('Feels like ' + pageData[6].feelsLikeFOne + '°F');
-        } else {
-          $('#cityOneLocation h1').text(pageData[6].currentTempCOne + '°C');
-          $('#cityOneLocation h4').text('Feels like ' + pageData[6].feelsLikeCOne + '°C');
-        }
-      }
-
     }
-
-
-    function changeLocation() {
-
-      let newCity = $('#newCity').val();
-      let newState = $('#newState').val();
-
-      pageData[0].userCity = newCity;
-      pageData[0].userState = newState;
-
-      // updateLocalStorage();
-      populateValuesOnPage();
-    }
-
-    function toggleFC() {
-      let userTempType = pageData[0].userTempType;
-
-      if (userTempType === 'F') {
-        pageData[0].userTempType = 'C';
-        $('#toggleFC').removeClass('btn-info').addClass('btn-warning');
-      } else if (userTempType === 'C'){
-        pageData[0].userTempType = 'F';
-        $('#toggleFC').removeClass('btn-warning').addClass('btn-info');
-      }
-
-      // updateLocalStorage();
-      populateValuesOnPage();
-    }
-
-    function compareCityOne(event){
-      event.preventDefault();
-      event.stopPropagation();
-
-      let cityOne = $('#cityOne').val();
-      let cityOneFormatted = cityOne.split(' ').join('_');
-      let stateOne = $('#stateOne').val();
-      let compareLocationOne = stateOne + '/' + cityOneFormatted;
-
-      let cityOneData = {
-        cityOne: cityOne,
-        stateOne: stateOne,
-      }
-
-      pageData.push(cityOneData);
-
-      let compareLocationOneURL = apiURL + 'conditions/q/' + compareLocationOne + '.json';
-
-      $.get(compareLocationOneURL)
-      .done(function(data){
-        // debugger;
-        let currentTempFOne = data.current_observation.temp_f
-        let currentTempCOne = data.current_observation.temp_c
-        let feelsLikeFOne = data.current_observation.feelslike_f
-        let feelsLikeCOne = data.current_observation.feelslike_c
-        let weatherIconURLOne = data.current_observation.icon_url
-
-        let conditionsDataOne = {
-          currentTempFOne: currentTempFOne,
-          currentTempCOne: currentTempCOne,
-          feelsLikeFOne: feelsLikeFOne,
-          feelsLikeCOne: feelsLikeCOne,
-          weatherIconURLOne: weatherIconURLOne
-        }
-
-        pageData.push(conditionsDataOne);
-
-        populateValuesOnPage();
-
-      }).fail(function(error){
-        console.log('compareOne error', error);
-      });
-    }
-
-
   }
+
+
+  function changeLocation() {
+
+    let newCity = $('#newCity').val();
+    let newState = $('#newState').val();
+
+    pageData[0].userCity = newCity;
+    pageData[0].userState = newState;
+
+    updateLocalStorage();
+    populateValuesOnPage();
+  }
+
+  function toggleFC() {
+    let userTempType = pageData[0].userTempType;
+
+    if (userTempType === 'F') {
+      pageData[0].userTempType = 'C';
+      $('#toggleFC').removeClass('btn-info').addClass('btn-warning');
+    } else if (userTempType === 'C'){
+      pageData[0].userTempType = 'F';
+      $('#toggleFC').removeClass('btn-warning').addClass('btn-info');
+    }
+
+    updateLocalStorage();
+    populateValuesOnPage();
+  }
+
+  function compareCityOne(event){
+    event.preventDefault();
+    event.stopPropagation();
+
+    let cityOne = $('#cityOne').val();
+    let cityOneFormatted = cityOne.split(' ').join('_');
+    let stateOne = $('#stateOne').val();
+    let compareLocationOne = stateOne + '/' + cityOneFormatted;
+
+    let cityOneData = {
+      cityOne: cityOne,
+      stateOne: stateOne,
+    }
+
+    pageData.push(cityOneData);
+
+    let compareLocationOneURL = apiURL + 'conditions/q/' + compareLocationOne + '.json';
+
+    $.get(compareLocationOneURL)
+    .done(function(data){
+      // debugger;
+      let currentTempFOne = data.current_observation.temp_f
+      let currentTempCOne = data.current_observation.temp_c
+      let feelsLikeFOne = data.current_observation.feelslike_f
+      let feelsLikeCOne = data.current_observation.feelslike_c
+      let weatherIconURLOne = data.current_observation.icon_url
+
+      let conditionsDataOne = {
+        currentTempFOne: currentTempFOne,
+        currentTempCOne: currentTempCOne,
+        feelsLikeFOne: feelsLikeFOne,
+        feelsLikeCOne: feelsLikeCOne,
+        weatherIconURLOne: weatherIconURLOne
+      }
+
+      pageData.push(conditionsDataOne);
+
+      updateLocalStorage();
+      populateValuesOnPage();
+
+    }).fail(function(error){
+      console.log('compareOne error', error);
+    });
+  }
+
+
 
 
 
